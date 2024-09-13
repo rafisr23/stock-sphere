66cf0d55c1bc80d6359b25f6d3effdbcb528e3b4
Abdur Rafi <rafiridwan23@gmail.com>
2024-09-13 10:37:30 +0700

    add datatables responsive js

--- resources/views/layouts/footerjs.blade.php
+++ resources/views/layouts/footerjs.blade.php
@@ -8,6 +8,8 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('build/js/plugins/dataTables.min.js') }}"></script>
 <script src="{{ URL::asset('build/js/plugins/dataTables.bootstrap5.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/plugins/dataTables.responsive.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/plugins/responsive.bootstrap5.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 @if (env('APP_DARK_LAYOUT') == 'default')
