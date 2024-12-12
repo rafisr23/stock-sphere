74e5c4e190d0c943220e02ad67485515f59473d2
Mario Chamdjoko Jati Mulyo <mario@digits.id>
2024-12-13 00:11:49 +0700

    Fix/Repairments to PDF
    Fix raw view for submission of repairs reports to PDF

--- resources/views/submission/toPDF.blade.php
+++ resources/views/submission/toPDF.blade.php
@@ -10,16 +10,34 @@
 
 <body>
     <h1>{{ $submission->unit->customer_name }}</h1>
+    <p>{{ $submission->unit->street }}</p>
+    <p>{{ $submission->unit->city }}, {{ $submission->unit->postal_code }}</p>
     <h2>{{ $submission->room->name }}</h2>
     @foreach ($detailsWithWorkHours as $d)
-        <p>Detail ID: {{ $d['detail']->id }}</p>
-        <p>Description: {{ $d['detail']->description }}</p>
-        <p>Remarks: {{ $d['detail']->remarks }}</p>
-        <p>Evidence: {{ $d['detail']->evidence }}</p>
-        <p>Technician's evidence:</p>
-        @foreach ($d['detail']->evidenceTechnician as $eT)
-            <p>{{ $eT->evidence }}</p>
-        @endforeach
+        <table class="table table-bordered">
+            <thead>
+                <th>Item name</th>
+                <th>Description</th>
+                <th>Remarks</th>
+                <th>Evidence</th>
+            </thead>
+            <tbody>
+                <td>{{ $d['detail']->itemUnit->items->item_name }}</td>
+                <td>{{ $d['detail']->description }}</td>
+                <td>{{ $d['detail']->remarks }}</td>
+                <td>{{ $d['detail']->evidence }}</td>
+            </tbody>
+            <thead>
+                <th>
+                    Evidence from technician
+                </th>
+            </thead>
+            <tbody>
+                @foreach ($d['detail']->evidenceTechnician as $eT)
+                    <p>{{ $eT->evidence }}</p>
+                @endforeach
+            </tbody>
+        </table>
         <p>Work Hours: {{ $d['workHours']['hours'] }} hours, {{ $d['workHours']['minutes'] }} minutes</p>
     @endforeach
 </body>
